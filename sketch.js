33cf1ce23c7b4174885d34250d283c04081fd673
aneetayadv <70881389+aneetayadv@users.noreply.github.com>
2021-03-18 12:48:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function preload() {
 }
 
 function setup(){
-    createCanvas(1000,800);
+    var cnv = createCanvas(windowWidth,windowHeight);
     
     tom = createSprite(870, 600);
     tom.addAnimation("tomSleeping", tomImg1);
